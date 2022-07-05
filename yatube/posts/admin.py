4f64339f75a6869aceafcd9f65820b116ec824a9
Андрей Гуртовой <andrej_gurtovoj@mail.ru>
2022-07-05 22:48:55 +0300

    Исправление шаблонов

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -18,3 +18,4 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
